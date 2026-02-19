17551e32377fe7bc7ff3ad7206b5ce00d64c2ecd
victorfontabelli-cyber <victorfontabelli@prof.educacao.sp.gov.br>
2026-02-19 13:07:21 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "busca-ativa-cache-v2";
+const CACHE_NAME = "busca-ativa-cache-v3";
 const urlsToCache = [
   "./",
   "./index.html",
@@ -39,3 +39,4 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
