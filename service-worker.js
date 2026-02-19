8b7ea489a0798c4d98a12843636ae5affb6d43f9
victorfontabelli-cyber <victorfontabelli@prof.educacao.sp.gov.br>
2026-02-19 12:16:07 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,16 @@
-const CACHE_NAME = "busca-ativa-jg-v2";
+const CACHE_NAME = "busca-ativa-jg-v3";
 
-const ASSETS = [
+const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./service-worker.js",
-  "./icon-192.png",
-  "./icon-512.png"
+  "./ICON-192.PNG",
+  "./ICON-512.PNG"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
@@ -19,7 +18,11 @@ self.addEventListener("install", (event) => {
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
     )
   );
   self.clients.claim();
@@ -27,8 +30,6 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return cached || fetch(event.request);
-    })
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
